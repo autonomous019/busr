c8bd933f170f74107dde41332aa70607885896c6
mike mccarron <m.j.h.mccarron@gmail.com>
2013-09-27 14:33:23 -0700

    web.js for local build

--- web.js
+++ web.js
@@ -32,8 +32,10 @@ app.get('/route/:id', function(req, res) {
 	res.render('route',{title:entry.title, busr:entry});
 });
 
-//app.listen(5000);
+app.listen(3000);
 
-app.listen(process.env.PORT || 5000, function(){
+//heroku specific
+/*app.listen(process.env.PORT || 5000, function(){
   console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
 });
+*/
